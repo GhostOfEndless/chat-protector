9efc88a50c65b58737c9cbf58b73650f5a026164
Aleksey Kabanov <phyrocycle.moscow@yandex.ru>
2024-09-23 12:45:03 +0300

    Common module added

--- update-receiver/src/main/java/org/example/receiver/UpdateReceiverApplication.java
+++ update-receiver/src/main/java/org/example/receiver/UpdateReceiverApplication.java
@@ -1,12 +1,12 @@
-package org.example.tgbot;
+package org.example.receiver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TelegramBotApplication {
+public class UpdateReceiverApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TelegramBotApplication.class, args);
+        SpringApplication.run(UpdateReceiverApplication.class, args);
     }
 }
